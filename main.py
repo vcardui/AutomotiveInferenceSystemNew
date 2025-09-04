99cc677d7adbf726b85ea62ffbb9c52cb3ee6756
vanessa <vanessa@reteguin.com>
2025-09-04 17:59:49 -0600

    def yocsan()

--- main.py
+++ main.py
@@ -78,6 +78,8 @@ def open_popup():
     submit_button = tk.Button(popup, text="Submit", command=submit_data)
     submit_button.grid(row=2, column=0, columnspan=2, pady=10)
 
+def yocsan():
+    pass
 
 # ------------------------- UI Set up ------------------------
 
